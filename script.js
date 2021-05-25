4c07476c5f5ae9c0c848026ad31308dfabac7e7e
Bepo Anticevic <60451596+Hrijeb@users.noreply.github.com>
2021-05-25 21:58:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function gameOver(gameWon) {
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
 	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "Reši kidaru.");
 }
 
 function declareWinner(who) {
